af8967645335717e78280a85a5a182685ef7a2d2
dmitru <dmitriy.borodiy@gmail.com>
2013-09-21 05:51:59 +0400

    All done

--- src/ru/ncedu/java/tasks/WordCounterImpl.java
+++ src/ru/ncedu/java/tasks/WordCounterImpl.java
@@ -1,8 +1,7 @@
 package ru.ncedu.java.tasks;
 
 import java.io.PrintStream;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,6 +12,7 @@ import java.util.Map;
  */
 public class WordCounterImpl implements WordCounter {
     private String text = null;
+    private Map<String, Long> dict = null;
 
     @Override
     public void setText(String text) {
@@ -29,26 +29,83 @@ public class WordCounterImpl implements WordCounter {
         if (this.text == null)
             throw new IllegalStateException();
 
-        return null;
+        if (this.dict == null)
+            this.dict = this.countWords(this.text);
+
+        return new HashMap<String, Long>(this.dict);
+    }
+
+    private Map<String, Long> countWords(String text) {
+        Map<String, Long> dict = new HashMap<String, Long>();
+
+        for (String word : text.split("[ \\t\\n]+")) {
+            word = word.toLowerCase();
+            if (word.matches("<.*>") || word.length() == 0)
+                continue;
+
+            Long counter = dict.get(word);
+            if (counter == null)
+                counter = new Long(1);
+            else
+                counter = counter + 1;
+
+            dict.put(word, counter);
+        }
+
+        return dict;
     }
 
     @Override
     public List<Map.Entry<String, Long>> getWordCountsSorted() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        if (this.text == null)
+            throw new IllegalStateException();
+
+        return this.sortWordCounts(this.getWordCounts());
     }
 
     @Override
     public List<Map.Entry<String, Long>> sortWordCounts(Map<String, Long> orig) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        List<Map.Entry<String, Long>> newList = new ArrayList<Map.Entry<String, Long>>();
+
+        for (Map.Entry<String, Long> entry : orig.entrySet()) {
+            newList.add(new AbstractMap.SimpleEntry<String, Long>(entry));
+        }
+
+        Collections.sort(newList, new Comparator<Map.Entry<String, Long>>() {
+            @Override
+            public int compare(Map.Entry<String, Long> o1, Map.Entry<String, Long> o2) {
+                if (o2.getValue() > o1.getValue())
+                    return 1;
+                else if (o2.getValue() < o1.getValue())
+                    return -1;
+                else
+                    return o1.getKey().compareTo(o2.getKey());
+            }
+        });
+
+        return newList;
     }
 
     @Override
     public void printWordCounts(PrintStream ps) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        if (this.text == null)
+            throw new IllegalStateException();
+
+        Map<String, Long> dict = this.getWordCounts();
+        for (Map.Entry<String, Long> entry : dict.entrySet()) {
+            ps.format("%s %d\n", entry.getKey(), entry.getValue());
+        }
     }
 
     @Override
     public void printWordCountsSorted(PrintStream ps) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        if (this.text == null)
+            throw new IllegalStateException();
+
+        List<Map.Entry<String, Long>> entries = this.sortWordCounts(this.getWordCounts());
+
+        for (Map.Entry<String, Long> entry : entries) {
+            ps.format("%s %d\n", entry.getKey(), entry.getValue());
+        }
     }
 }
--- src/ru/ncedu/java/tasks/WordCounterImplTest.java
+++ src/ru/ncedu/java/tasks/WordCounterImplTest.java
@@ -1,5 +1,11 @@
 package ru.ncedu.java.tasks;
 
+import junit.framework.Assert;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: dborody
@@ -10,32 +16,91 @@ package ru.ncedu.java.tasks;
 public class WordCounterImplTest {
 
     @org.junit.Test
-    public void testGetText() throws Exception {
+    public void testSetGetText() throws Exception {
+        final String s = "a a b c d e";
+        WordCounter counter = new WordCounterImpl();
 
+        counter.setText("a a b c d e");
+        Assert.assertEquals(s, counter.getText());
     }
 
     @org.junit.Test
     public void testGetWordCounts() throws Exception {
+        WordCounter counter = new WordCounterImpl();
 
+        try {
+            counter.getWordCounts();
+            Assert.fail();
+        } catch (Exception e) {
+
+        }
+
+        final String s = "<> a    <a>  A <not-a-word>  b\t<not-really>> \t  d c  d a";
+        counter.setText(s);
+
+        Map<String, Long> expected = new HashMap<String, Long>();
+        expected.put("a", new Long(3));
+        expected.put("b", new Long(1));
+        expected.put("c", new Long(1));
+        expected.put("d", new Long(2));
+
+        Assert.assertEquals(expected, counter.getWordCounts());
     }
 
     @org.junit.Test
     public void testGetWordCountsSorted() throws Exception {
+        WordCounter counter = new WordCounterImpl();
 
-    }
+        try {
+            counter.getWordCounts();
+            Assert.fail();
+        } catch (Exception e) {
 
-    @org.junit.Test
-    public void testSortWordCounts() throws Exception {
+        }
 
+        final String s = "a     A  aB\t\t  d Ac  d a";
+        counter.setText(s);
+
+        List<Map.Entry<String, Long>> expected = new LinkedList<Map.Entry<String, Long>>();
+        expected.add(new AbstractMap.SimpleEntry<String, Long>("a", new Long(3)));
+        expected.add(new AbstractMap.SimpleEntry<String, Long>("d", new Long(2)));
+        expected.add(new AbstractMap.SimpleEntry<String, Long>("ab", new Long(1)));
+        expected.add(new AbstractMap.SimpleEntry<String, Long>("ac", new Long(1)));
+
+        Assert.assertEquals(expected, counter.getWordCountsSorted());
     }
 
     @org.junit.Test
     public void testPrintWordCounts() throws Exception {
+        WordCounter counter = new WordCounterImpl();
 
+        final String s = "\t\n a  d D  A  d aB\t\t  d Ac  d a";
+        counter.setText(s);
+
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(buffer);
+
+        counter.printWordCounts(ps);
+        Assert.assertEquals("d 5\n" +
+                "a 3\n" +
+                "ac 1\n" +
+                "ab 1\n", buffer.toString());
     }
 
     @org.junit.Test
     public void testPrintWordCountsSorted() throws Exception {
+        WordCounter counter = new WordCounterImpl();
 
+        final String s = " a  d D  A  d aB\t\t  d Ac  d a\n\n\n  ";
+        counter.setText(s);
+
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(buffer);
+
+        counter.printWordCountsSorted(ps);
+        Assert.assertEquals("d 5\n" +
+                "a 3\n" +
+                "ab 1\n" +
+                "ac 1\n", buffer.toString());
     }
 }
